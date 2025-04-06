ffa7a4ea1e652857c444b73204b06d0b70a90fd5
Infy Lul <charithankam6@gmail.com>
2025-04-06 14:01:15 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from vtp import vision_transformer,config
+from flask import Flask, jsonify, request, render_template
+
+from torchvision import transforms
+
+from PIL import Image
+
+import torch
+import torch.quantization
+
+print(torch.__version__)
+transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+model = vision_transformer(config)
+
+model.load_state_dict(torch.load("vit_model_MP1.pth",weights_only=True,map_location=torch.device('cpu')))
+
+model.eval()
+
+app = Flask(__name__)
+
+dic = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
+
+def predict_label(img_path):
+	i = Image.open(img_path)
+	i = transform(i)
+	p = model(i.unsqueeze(dim=0))
+	_,pred = p.max(1)
+	return dic[pred]
+
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("index.html")
+
+@app.route("/about")
+def about_page():
+	return "Plant disease predictor"
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	#"static/"
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/"+img.filename	
+		img.save(img_path)
+
+		p = predict_label(img_path)
+
+	return render_template("index.html", prediction = p, img_path = img_path)
+
+if __name__ =='__main__':
+	#app.debug = True
+	app.run(debug = True)
